c635aa299ef81b70a6c5638fb1fe9d24789b136c
Malorie <malorie18@gmail.com>
2018-06-20 17:59:10 -0400

    working version

--- mod-2-project-instaclone/app/assets/javascripts/application.js
+++ mod-2-project-instaclone/app/assets/javascripts/application.js
@@ -11,8 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap
 //= require rails-ujs
-//= require activestorage
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
